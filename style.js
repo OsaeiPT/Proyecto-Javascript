52beca61342ce8aef194b2fc2fe3cc21bf5304e9
OsaeiPT <osaei.p@viacodigo.com>
2020-01-21 11:48:13 -0500

    subiendo JS

--- style.js
+++ style.js
@@ -1,6 +1,7 @@
-var nombre = "Hola Mundo De JavaScript";
-var numero = "20";
-var decimal = "23.5";
+var edad = "18";
+var boton = document.getElementById("boton")
+var registrate = document.getElementById("registrate")
+var informacion = "23.5";
 var texto = "Hola, Pagina De Practica";
 var subtexto = "true";
 var subtexto1 = "false";
@@ -13,41 +14,103 @@ var planta = {
   tamaño: "grande",
   cantidad: 3
 }
-var miFuncion = function () { };
+var generador = document.getElementById("generador");
 
-// Pagina De Prueba Comienza
+
+
+// Primer Script
+
+var nombre = "Hola Mundo De JavaScript";
+console.log(nombre);
+
+// Segundo Script
+
+var edad = "tengo 18 años";
+console.log(edad);
+
+// Segundo Script
 
 var title = document.querySelector("Titulo");
 var boton = document.getElementById("boton");
 
-// Nodos
+// Tercer Script
 
 var parrafoNuevo = document.createElement("p");
-var textoNuevo = document.createTextNode("Primer Parrafo Creado Con JS");
+var textoNuevo = document.createTextNode("Grasias Por El Curso De JavaScript");
 parrafoNuevo.appendChild(textoNuevo);
 parrafoNuevo.setAttribute("class", "texto-js");
 var contenedor = document.getElementsByClassName("contenedor")[0];
 contenedor.appendChild(parrafoNuevo);
 
-// Canbiando Nombres
+// Cuarto Script
 
 var segundoParrafo = document.getElementById("texto2");
 segundoParrafo.className = "texto2 segundo"
 var primerParrafo = document.getElementsByClassName("texto1")[0];
 
-// Siguendo
+// Quinto Script
 
-var parrafo = document.createElement("p");
-var textoNuevo = document.createTextNode("Segundo Parrafo Creado Con JS")
+function alerta() {
+  alert("Esto Puede Tardar Un Poco !");
+}
+
+boton.addEventListener("click", alerta)
+
+registrate.addEventListener("click", alerta)
+
+// Eliminar Escuchador
+
+function removerAlerta() {
+  boton.removeEventListener("click", alerta);
+  registrate.removeEventListener("click", alerta);
+}
+
+boton.addEventListener("click", removerAlerta)
+registrate.addEventListener("click", removerAlerta)
+
+// Sexto Script
 
 
+generador.addEventListener("click", function () {
+  var nuevoParrafo = document.createElement("p");
+  var textoNuevo = document.createTextNode("Sigue Dandole Click");
 
+  nuevoParrafo.appendChild(textoNuevo)
+  nuevoParrafo.className = "text-info_concept";
 
+  var contenedor = document.getElementsByClassName("contenedor")[0];
+  contenedor.appendChild(nuevoParrafo);
 
+})
 
+// Septimo Script  
 
+var parrafos = document.getElementsByClassName("parrafo")
+rojo = document.getElementById("boton_rojo")
+violeta = document.getElementById("boton_violeta")
+bgamarillo = document.getElementById("boton_amarillo")
+bgdark = document.getElementById("boton_negro")
 
+rojo.addEventListener("click", function (e) {
+  for (var i = 0; i < parrafos.length; i++) {
+    parrafos[i].style.color = "red";
+  }
+});
 
+violeta.addEventListener("click", function (e) {
+  for (var i = 0; i < parrafos.length; i++) {
+    parrafos[i].style.color = "violet";
+  }
+});
 
+bgamarillo.addEventListener("click", function (e) {
+  for (var i = 0; i < parrafos.length; i++) {
+    parrafos[i].style.background = "yellow";
+  }
+});
 
-console.log("Hola Mundo De JavaScript");
+bgdark.addEventListener("click", function (e) {
+  for (var i = 0; i < parrafos.length; i++) {
+    parrafos[i].style.background = "black";
+  }
+});
